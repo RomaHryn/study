25916f49adcc3c4908e3a938cafc0248e73d30b0
RomaHryn <smkroma@gmail.com>
2016-06-12 11:15:20 +0300

    initial project version

---
+++ script.js
@@ -0,0 +1,22 @@
+var arr = [{color:'red', isActive:false, isVisible:true},
+{color:'blue', isActive:false, isVisible:true},
+{color:'green', isActive:false, isVisible:true},
+{color:'gray', isActive:false, isVisible:true},
+{color:'orange', isActive:false, isVisible:true},
+{color:'black', isActive:false, isVisible:true},
+{color:'yellow', isActive:false, isVisible:true},
+{color:'pink', isActive:false, isVisible:true},
+{color:'red', isActive:false, isVisible:true},
+{color:'blue', isActive:false, isVisible:true},
+{color:'green', isActive:false, isVisible:true},
+{color:'gray', isActive:false, isVisible:true},
+{color:'orange', isActive:false, isVisible:true},
+{color:'black', isActive:false, isVisible:true},
+{color:'yellow', isActive:false, isVisible:true},
+{color:'pink', isActive:false, isVisible:true}];
+
+
+for (var i = 0; i < arr.length; i++){
+
+}
+console.log ( arr );
